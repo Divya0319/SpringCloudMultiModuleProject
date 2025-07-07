0d6142177b7dd24ffb6e4559fc516879f8c20b42
Divya Gupta <divygupta0319@gmail.com>
2025-07-07 17:28:54 +0530

    Added fortune-config-server module

---
+++ fortune-config-server/src/test/java/com/fastturtle/fortuneconfigserver/FortuneConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastturtle.fortuneconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
